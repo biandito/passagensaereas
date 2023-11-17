b682bcb24a2713d2fe3a44d73f7b7156bba49141
biandito <bekatrivz@gmail.com>
2023-11-17 19:34:06 -0300

    update

--- src/App.java
+++ src/App.java
@@ -38,12 +38,16 @@ public class App {
 	    		 String model = JOptionPane.showInputDialog("Digite o modelo do Aviao");
 	    		 for(int i=0; i< nAero; i++) if(model.equals(aero[i].modelo)) {JOptionPane.showMessageDialog(null, "Modelo existe");}
 	    		 int lugares[] = new int[2];
-	    		 lugares[0] = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de fileiras"));
-	    		 lugares[1] = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de assentos por fileira"));
-	    		 aero[nAero] = new Aviao(model,lugares[0],lugares[1]);
-	    		 
+				 try {
+					lugares[0] = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de fileiras"));
+					lugares[1] = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de assentos por fileira"));
+				} catch (NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "Erro ao ler os dados. Certifique-se de inserir números válidos.");
+					return; 
+				} finally {
+					
 	    		 JOptionPane.showMessageDialog(null, aero[nAero].getModelo());
-	    		 nAero++;
+	    		 nAero++; }
 	    		 break;
 	    		 
 	    	 //CADASTRO DE VOO
@@ -56,7 +60,8 @@ public class App {
 	    		 for(int j = 0; j<nAero; j++) {if(aeroVooModel==aero[j].modelo) {aeroVoo= aero[j];}}*/
 	    		 Aviao aeroVoo = new Aviao();
 	    		 aeroVoo.setAero(nAero, aero);
-	    		 String mesVoo = JOptionPane.showInputDialog("Qual mês?");
+				 try {
+				 String mesVoo = JOptionPane.showInputDialog("Qual mês?");
 	    		 String diaVoo = JOptionPane.showInputDialog("Qual dia?");
 	    		 String horaVoo = JOptionPane.showInputDialog("Qual hora?");
 	    		 voos[nVoo] = new Voo(aeroVoo, nVoo, diaVoo+"/"+mesVoo, horaVoo);
@@ -69,11 +74,16 @@ public class App {
 	    				 +"\n Assentos: "+ voos[nVoo].aeronave.Lugares[0].length);
 	    		 
 	    		 nVoo++;
-	    		 
+				} catch (NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "Erro ao ler dados numéricos. Certifique-se de inserir números válidos.");
+					return;
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Erro desconhecido ao cadastrar voo.");
+					return;
+				}
+				break; 
 
-	    		 
-	    		 break;
-	    	 }
+	    		}
 	    	 break;
 	    	 
 	    //MENU RESERVA DE PASSAGENS
@@ -85,50 +95,72 @@ public class App {
 	    	 switch(opcao[2]) {
 	    	 
 	    	 //FAZER RESERVA
-	    	 case 0:
-	    		 String vooStr[] = new String[nVoo];
-	    		 for(int i = 0; i<nVoo; i++) {vooStr[i]=voos[i].aeronave.modelo+", "+ voos[i].data+", " + voos[i].hora;}
-	    		 String escVooStr = (String) JOptionPane.showInputDialog(null, "Escolha um voo", "Escolher Voo", nVoo, null, vooStr, vooStr[0]);
-	    		 Voo escVoo = new Voo();
-
-	    		 for(int j = 0; j<nVoo; j++) {if((voos[j].aeronave.modelo+", "+ voos[j].data+", " + voos[j].hora).equals(escVooStr)){
-	    			 escVoo= voos[j];}}
-	    		 
-	    		 String nome, cpf;
-	    		 nome = JOptionPane.showInputDialog("Digite seu nome");
-	    		 cpf = JOptionPane.showInputDialog("Digite seu cpf");
-	    		 Passageiro passa = new Passageiro(nome, cpf);
-	    		 int lugar[] = new int[2];
-	    		 lugar[0]= Integer.parseInt(JOptionPane.showInputDialog(null, "Qual fileira?"));
-	    		 lugar[1]= Integer.parseInt(JOptionPane.showInputDialog(null, "Qual assento?"));
-	    		 escVoo.aeronave.setPassageiro(lugar[0]-1, lugar[1]-1, passa);
-	    		 break;
+			 case 0:
+			 String vooStr[] = new String[nVoo];
+			 for (int i = 0; i < nVoo; i++) {
+				 vooStr[i] = voos[i].aeronave.modelo + ", " + voos[i].data + ", " + voos[i].hora;
+			 }
+			 String escVooStr = (String) JOptionPane.showInputDialog(null, "Escolha um voo", "Escolher Voo", nVoo, null, vooStr, vooStr[0]);
+			 Voo escVoo = new Voo();
+		 
+			 for (int j = 0; j < nVoo; j++) {
+				 if ((voos[j].aeronave.modelo + ", " + voos[j].data + ", " + voos[j].hora).equals(escVooStr)) {
+					 escVoo = voos[j];
+				 }
+			 }
+ 		 
+			 String nome, cpf;
+			 nome = JOptionPane.showInputDialog("Digite seu nome");
+			 cpf = JOptionPane.showInputDialog("Digite seu cpf");
+			 int lugar[] = new int[2];
+		 
+			 try {
+				 lugar[0] = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual fileira?"));
+				 lugar[1] = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual assento?"));
+			 } catch (NumberFormatException e) {
+				 JOptionPane.showMessageDialog(null, "Erro ao ler dados numéricos. Certifique-se de inserir números válidos.");
+				 return;
+			 } finally {
+				 escVoo.aeronave.setPassageiro(lugar[0] - 1, lugar[1] - 1, new Passageiro(nome, cpf));
+			 }
+			 break;		 
 	    		 
 	    	 //CONSULTAR NUMERO DE LUGARES VAZIOS
-	    	 case 1:
-	    		 String verVooStr[] = new String[nVoo];
-	    		 for(int i = 0; i<nVoo; i++) {verVooStr[i]=voos[i].aeronave.modelo+", "+ voos[i].data+", " + voos[i].hora;}
-	    		 String verStr = (String) JOptionPane.showInputDialog(null, "Escolha um voo", "Escolher Voo", nVoo, null, verVooStr, verVooStr[0]);
-	    		 Voo verVoo = new Voo();
-	    		 for(int j = 0; j<nVoo; j++) {if((voos[j].aeronave.modelo+", "+ voos[j].data+", " + voos[j].hora).equals(verStr)){
-	    			 verVoo= voos[j];}}
-	    		 /*Voo verVoo = (Voo) JOptionPane.showInputDialog(null, "Escolha um voo", "Escolher Voo", nVoo, null, voos, voos[0]);*/
-	    		 int nLugares = 0;
-	    		 for(int i = 0; i< verVoo.aeronave.Lugares.length; i++) {
-	    			 for(int j = 0; j<verVoo.aeronave.Lugares[0].length; j++) {
-	    				 
-	    				 if(!verVoo.aeronave.verificaLugarOcupado(i, j)) {nLugares++;}
-	    				 
-	    			 }
-	    			 
-	    		 }
-	    		 
-	    		 JOptionPane.showMessageDialog(null, "Numero de lugares livres: "+nLugares);
-	    		 
-
-
-	    		 
-	    		 break;
+			 case 1:
+			 try {
+				 String verVooStr[] = new String[nVoo];
+				 for (int i = 0; i < nVoo; i++) {
+					 verVooStr[i] = voos[i].aeronave.modelo + ", " + voos[i].data + ", " + voos[i].hora;
+				 }
+		 
+				 String verStr = (String) JOptionPane.showInputDialog(
+						 null, "Escolha um voo", "Escolher Voo", nVoo, null, verVooStr, verVooStr[0]);
+		 
+				 Voo verVoo = new Voo();
+				 for (int j = 0; j < nVoo; j++) {
+					 if ((voos[j].aeronave.modelo + ", " + voos[j].data + ", " + voos[j].hora).equals(verStr)) {
+						 verVoo = voos[j];
+					 }
+				 }
+		 		int nLugares = 0;
+				 for (int i = 0; i < verVoo.aeronave.Lugares.length; i++) {
+					 for (int j = 0; j < verVoo.aeronave.Lugares[0].length; j++) {
+						 if (!verVoo.aeronave.verificaLugarOcupado(i, j)) {
+							 nLugares++;
+						 }
+					 }
+				 }
+		 
+				 JOptionPane.showMessageDialog(null, "Numero de lugares livres: " + nLugares);
+			 } catch (NumberFormatException e) {
+				 JOptionPane.showMessageDialog(null, "Erro ao ler dados. Certifique-se de inserir números válidos.");
+				 return;
+			 } catch (Exception e) {
+				 JOptionPane.showMessageDialog(null, "Erro ao consultar lugares vazios.");
+				 return;
+			 } finally {
+			}
+			 break;
 	    		 
 	    		 
 	    	 //CONSULTAR RESERVAS
